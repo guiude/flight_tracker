0e45bb24e8ec5304c5b8b0b5832cd8e315efa5e9
Guilherme Ude Braz <guiude@gmail.com>
2022-11-21 11:51:56 -0300

    main and flighttracker merge

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import uvicorn
+from fastapi import FastAPI
+from flightdata import get_flight_data
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"msg": "Hello, World"}
+
+
+@app.get("/flights/{flight_code}")
+async def retrieve_flight(flight_code):
+    data = get_flight_data(flight_code)
+    return data
 
 if __name__ == '__main__':
-    uvicorn.run('flighttracker:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('flighttracker:app', host='0.0.0.0', port=80, reload=True)
